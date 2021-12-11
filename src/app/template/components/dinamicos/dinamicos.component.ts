6e68ba6c20c6ddf0ceded18a04a411630e7a7211
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-11 12:44:47 -0600

    formularios dinámicos - agregar elementos

--- src/app/template/components/dinamicos/dinamicos.component.ts
+++ src/app/template/components/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +18,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  // Modelo
+  persona: Persona = {
+    nombre: 'Alejandro',
+    favoritos: [
+      {id: 1, nombre: "Matal Slug"},
+      {id: 2, nombre: "Mario Bros 3"}
+    ]
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log('Posteado')
   }
 
 }
