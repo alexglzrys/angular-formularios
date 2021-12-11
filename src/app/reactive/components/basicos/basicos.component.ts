ed27e8f18dfd2d3f845ba6b6e9467ba89de4d298
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-11 15:36:29 -0600

    formularios complejos con FormBuilder

--- src/app/reactive/components/basicos/basicos.component.ts
+++ src/app/reactive/components/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,13 +9,25 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  // Esquema para formualarios reactivos Básicos en estructura
   // Crear una instancia de mi formulario reactivo
-  miFormularioBasico: FormGroup = new FormGroup({
+  /*miFormularioBasico: FormGroup = new FormGroup({
     // Indicar los controles o campos que gestionará este formulario
-    'nombre': new FormControl('RTX 4080ti')
+    nombre: new FormControl('RTX 4080ti'),
+    precio: new FormControl(1500),
+    existencias: new FormControl(4)
+  })*/
+
+  // Estructura para formularios reactivos Complejos en estructura
+  // Es necesario inyectar el servicio de FormBuilder para proceder a construir la estructura de nuestro formulario
+  miFormularioBasico: FormGroup = this.fb.group({
+    nombre: ['RTX 4080ti'],
+    precio: [15200],
+    existencias: [4]
   })
 
-  constructor() { }
+  // Inyectar servicio de FormBuilder
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
