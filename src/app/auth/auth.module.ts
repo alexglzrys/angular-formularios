669207d2c359cb807730de795775209e3384e699
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-11 21:34:16 -0600

    validar control de formulario contra una expresión regular

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // Expresión regular a validar
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.fb.group({
+    // Validar campo contra validaciones de angular y contra expresiones regulares
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoEsInvalido(nombreCampo: string): boolean | undefined {
+    // En formularios reactivos podemos acceder facilmente a la instancia de un control mediante el método get()
+    return this.miFormulario.get(nombreCampo)?.invalid && this.miFormulario.get(nombreCampo)?.touched;
+  }
+
+  registrar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+
+    console.log(this.miFormulario.value)
   }
 
 }
