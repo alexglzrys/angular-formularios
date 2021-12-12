e7ab70ae422bde13d32f03f10893c704dd5ff852
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-11 20:14:57 -0600

    formularios reactivos con Switches, radios y checks

--- src/app/reactive/components/switches/switches.component.ts
+++ src/app/reactive/components/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue] // Necesitamos que acepte las condiciones (required - pasa si hay un valor. false es un valor)
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.miFormulario.reset(this.persona)
+    // Como el objeto persona no tiene las condiciones, por defecto se establecen en NULL
+
+    // Una forma de colocar un valor por defecto al momento de inicializar mi formulario reactivo
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    })
+
+    // Al trabajar con formularios reactivos, puede suscribirme a todo el formulario o a uno de sus campos para escuchar cambios y proceder ante ello
+    this.miFormulario.valueChanges.subscribe(newValues => {
+      console.log(newValues);
+      // Por si se llegara a necesitar:
+      // En este punto puedo igualar el objeto persona al nuevo valor que viene desde el formulario reactivo
+      const personaRegistrada = newValues
+      delete personaRegistrada.condiciones; // no me interesa guardar una referencia hacia las condiciones
+      this.persona = personaRegistrada
+    })
+
+    // Esta es una suscripción cuando cambia el valor de un control especifico de formulario reactivo
+    this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValueConditions => {
+      console.log('Las condiciones han cambiado', newValueConditions)
+    })
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) return;
+
+    console.log(this.miFormulario.value)
+
+    // Por si se llegara a necesitar:
+    // En este punto puedo igualar el objeto persona al valor que viene desde el formulario reactivo
+    const personaRegistrada = this.miFormulario.value
+    delete personaRegistrada.condiciones; // no me interesa guardar una referencia hacia las condiciones
+    this.persona = personaRegistrada
   }
 
 }
